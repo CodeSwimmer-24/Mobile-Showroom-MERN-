25484726ee3dc0454514b490476029440fb3ee2e
Fahad <fahadmahmood1200@gmail.com>
2021-10-15 23:01:23 +0530

    Order api compleated

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -56,3 +56,73 @@ exports.myOrders = CatchAsyncError(async(req,res,next) => {
         orders,
     });
 });
+
+// Get All order -- Admin
+exports.getAllOrders = CatchAsyncError(async(req,res,next) => {
+
+    const orders = await Order.find();
+
+    let totalAmount = 0;
+
+    orders.forEach(order => {
+        totalAmount += order.totalPrice;
+    });
+
+    res.status(200).json({
+        success:true,
+        totalAmount,
+        orders,
+    });
+});
+
+// Update Order Status -- Admin
+exports.updateOrder = CatchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+  
+    if (!order) {
+      return next(new ErrorHandler("Order not found with this Id", 404));
+    }
+  
+    if (order.orderStatus === "Delivered") {
+      return next(new ErrorHandler("You have already delivered this order", 400));
+    }
+  
+    if (req.body.status === "Shipped") {
+      order.orderItems.forEach(async (o) => {
+        await updateStock(o.product, o.quantity);
+      });
+    }
+    order.orderStatus = req.body.status;
+  
+    if (req.body.status === "Delivered") {
+      order.deliveredAt = Date.now();
+    }
+  
+    await order.save({ validateBeforeSave: false });
+    res.status(200).json({
+      success: true,
+    });
+  });
+  
+  async function updateStock(id, quantity) {
+    const product = await Product.findById(id);
+  
+    product.Stock -= quantity;
+  
+    await product.save({ validateBeforeSave: false });
+  }
+
+// Delete order -- Admin
+exports.deleteOrder = CatchAsyncError(async(req,res,next) => {
+
+    const order = await Order.findByIdAndDelete(req.params.id);
+
+    if(!order){
+        return next(new ErrorHandler("Order not found with this Id",404));
+    }
+
+    res.status(200).json({
+        success:true,
+        message:"Order Deletated Sucessifully",
+    });
+});
