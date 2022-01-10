3c919f2590cc81d9cae09fd02db5c378e635eea4
CodeSwimmer-24 <fahadmahmood1200@gmail.com>
2022-01-10 10:40:05 -0500

    Added demo home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import { Expo } from 'gsap/all';
+import { TweenMax } from 'gsap/gsap-core';
+import { useEffect } from 'react';
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+    <Navbar />
+    <Home />
+  </>
   );
 }
 
